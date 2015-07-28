8172643aedef2326dcbfb35ff302bd8598b46498
tviskaron <tviskaron@gmail.com>
2015-07-28 16:21:00 +0300

    Snake

---
+++ main_snake.py
@@ -0,0 +1,27 @@
+from htm__region import Region
+from settings import *
+import os
+from input_generators import MakeBubble
+
+# generator = GENERATOR(REGION_SIZE_N)
+SCALE = 1
+generator = Snake(REGION_SIZE_N)
+
+r = Region(REGION_SIZE_N * SCALE, 5)
+print("""
+**** ЛЕГЕНДА *****
+P1 - Клетка с номером 1, данной колонки находится в состоянии предсказания
+A3 - Клетка с номером 3, данной колонки активировалась
+O3 - Клетка с номером 3, данной колонки активировалась из-за долгого простоя (PassiveTime > PASSIVE_TIME_TO_ACTIVE_THRESHOLD)
+""")
+
+for i in range(STEPS_NUMBER):
+    os.system('clear')
+    print('---------------------')
+    generator.out()
+    r.step_forward(generator.get_data())
+    r.out_prediction()
+    generator.move()
+    if generator.game_over:
+        generator = Snake(REGION_SIZE_N)
+    input()
