346a9e46c6176548b47ccf2f5fdd0b47484ba7d3
DIdro <gmdidro@gmail.com>
2015-10-07 11:24:39 +0300

    Экспенирмент с 2мя уровнями HTM

--- apps/settings.py
+++ apps/settings.py
@@ -27,19 +27,19 @@ class TemporalSettings:
         self.SYNAPSE_THRESHOLD = 0.25
         self.DENDRITE_PERMANENCE_INC_DELTA = 0.02
         self.DENDRITE_PERMANENCE_DEC_DELTA = -0.1
-        self.DENDRITE_ACTIVATE_THRESHOLD = 1
+        self.DENDRITE_ACTIVATE_THRESHOLD = 3
         self.PASSIVE_TIME_TO_ACTIVE_THRESHOLD = 2000
 
 class SpatialSettings:
     def __init__(self):
-        self.debug = True
+        self.debug = False
         self.min_overlap = 1
-        self.desired_local_activity = 3
+        self.desired_local_activity = 9
         self.connected_pct = 1
         self.connected_perm = 0.01
         self.xinput = 3
         self.yinput = 3
-        self.potential_radius = 2
+        self.potential_radius = 4
         self.xdimension = 9
         self.ydimension = 9
         self.initial_inhibition_radius = 1
