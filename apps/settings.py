0bf4ca96655efe2cde22981446d51ed06064efe6
tviskaron <tviskaron@gmail.com>
2015-12-01 11:07:35 +0300

    иерархия - начало

--- apps/settings.py
+++ apps/settings.py
@@ -5,8 +5,8 @@ from spatialPooler.mappers.sp_square_mapper_auto_radius import SquareMapperAutoR
 class InputSettings:
     def __init__(self):
         # раздувание
-        self.SCALE = 3
-        self.STEPS_NUMBER = 50000
+        self.SCALE = 1
+        self.STEPS_NUMBER = 900
 
         # GENERATOR = HardSteps
         # GENERATOR = ConstantActiveBit
@@ -21,13 +21,13 @@ class TemporalSettings:
         self.REGION_SIZE_N = 3
 
         # количество клеток в колонке
-        self.COLUMN_SIZE = 3
+        self.COLUMN_SIZE = 4
 
         self.INITIAL_PERMANENCE = 0.30
         self.SYNAPSE_THRESHOLD = 0.25
         self.DENDRITE_PERMANENCE_INC_DELTA = 0.02
         self.DENDRITE_PERMANENCE_DEC_DELTA = -0.1
-        self.DENDRITE_ACTIVATE_THRESHOLD = 45
+        self.DENDRITE_ACTIVATE_THRESHOLD = 1
         self.PASSIVE_TIME_TO_ACTIVE_THRESHOLD = 2000
 
 
@@ -35,14 +35,14 @@ class SpatialSettings:
     def __init__(self):
         self.debug = False
         self.min_overlap = 1
-        self.desired_local_activity = 9
+        self.desired_local_activity = 4
         self.connected_pct = 1
         self.connected_perm = 0.01
         self.xinput = 3
         self.yinput = 3
         self.potential_radius = 4
-        self.xdimension = 9
-        self.ydimension = 9
+        self.xdimension = 3
+        self.ydimension = 3
         self.initial_inhibition_radius = 1
         self.permanence_inc = 0.1
         self.permanence_dec = 0.1
