6e5d54996f89136e8d511b66373c709882dfc952
DIdro <gmdidro@gmail.com>
2015-12-10 11:08:02 +0300

    Странное поведение (см. main.py). При несовпадающей размерности SP предсказывается чаще.

--- apps/settings.py
+++ apps/settings.py
@@ -6,7 +6,7 @@ from spatialPooler.mappers.sp_square_mapper_auto_radius import SquareMapperAutoR
 class InputSettings:
     def __init__(self):
         # раздувание
-        self.SCALE = 1
+        self.SCALE = 2
         self.STEPS_NUMBER = 900
 
         # self.GENERATOR = HardSteps
@@ -19,11 +19,10 @@ class InputSettings:
 
 class TemporalSettings:
     def __init__(self):
-        # раздувание
-        self.REGION_SIZE_N = 3
+        self.REGION_SIZE_N = 5
 
         # количество клеток в колонке
-        self.COLUMN_SIZE = 4
+        self.COLUMN_SIZE = 8
 
         self.INITIAL_PERMANENCE = 0.30
         self.SYNAPSE_THRESHOLD = 0.25
