628759a007725d30a77da0708e71db7d4436706e
tviskaron <tviskaron@gmail.com>
2016-01-28 19:49:33 +0300

    в draw_graph_vis убрал принты добавил with для записи в файл

--- dendrite_graph/draw_graph_vis.py
+++ dendrite_graph/draw_graph_vis.py
@@ -13,78 +13,76 @@ def draw_graph(file_name, input_edges):
         if not y in mm:
             mm[y] = cnt
             cnt += 1
-    for i in mm:
-        print(mm[i], i)
-    print()
+    # for i in mm:
+    #     print(mm[i], i)
+    # print()
     edges_str = "["
     for i in input_edges:
         x, y = i
-        print(mm[x], mm[y])  # ребро
+        # print(mm[x], mm[y])  # ребро
         edges_str = edges_str + "[" + str(mm[x]) + "," + str(mm[y]) + "],"
     edges_str += "]"
     edges = edges_str
     nodes = json.encode(mm)
-
-
-    f = open(file_name, 'w')
     s = """
-<!doctype html>
-<html>
-<head>
-  <title>Network | Basic usage</title>
+    <!doctype html>
+    <html>
+    <head>
+      <title>Network | Basic usage</title>
 
-  <script type="text/javascript" src="vis.js"></script>
-  <link href="vis.css" rel="stylesheet" type="text/css" />
+      <script type="text/javascript" src="vis.js"></script>
+      <link href="vis.css" rel="stylesheet" type="text/css" />
 
-  <style type="text/css">
-    #mynetwork {
-      width: 600px;
-      height: 400px;
-      border: 1px solid lightgray;
-    }
-  </style>
-</head>
-<body>
+      <style type="text/css">
+        #mynetwork {
+          width: 600px;
+          height: 400px;
+          border: 1px solid lightgray;
+        }
+      </style>
+    </head>
+    <body>
 
-<p>
-  Create a simple network with some nodes and edges.
-</p>
+    <p>
+      Create a simple network with some nodes and edges.
+    </p>
 
-<div id="mynetwork"></div>
+    <div id="mynetwork"></div>
 
-<script type="text/javascript">
-  var nodes_dict = """ + nodes + """
-  var edges_list = """ + edges + """
- var nodes_visjs=[]
-  // create an array with nodes
-  for(n in nodes_dict)
-	nodes_visjs.push({id:nodes_dict[n],label: n})
+    <script type="text/javascript">
+      var nodes_dict = """ + nodes + """
+      var edges_list = """ + edges + """
+     var nodes_visjs=[]
+      // create an array with nodes
+      for(n in nodes_dict)
+        nodes_visjs.push({id:nodes_dict[n],label: n})
 
-  var nodes = new vis.DataSet(nodes_visjs);
+      var nodes = new vis.DataSet(nodes_visjs);
 
-  var edges_visjs=[]
-  	console.log(edges_list)
-  for(n in edges_list)
-  {
-	edges_visjs.push({from:edges_list[n][0],to: edges_list[n][1]})
-	console.log(n)
-  }
+      var edges_visjs=[]
+        console.log(edges_list)
+      for(n in edges_list)
+      {
+        edges_visjs.push({from:edges_list[n][0],to: edges_list[n][1]})
+        console.log(n)
+      }
 
-  // create an array with edges
-  var edges = new vis.DataSet(edges_visjs);
+      // create an array with edges
+      var edges = new vis.DataSet(edges_visjs);
 
-  // create a network
-  var container = document.getElementById('mynetwork');
-  var data = {
-    nodes: nodes,
-    edges: edges
-  };
-  var options = {};
-  var network = new vis.Network(container, data, options);
-</script>
+      // create a network
+      var container = document.getElementById('mynetwork');
+      var data = {
+        nodes: nodes,
+        edges: edges
+      };
+      var options = {};
+      var network = new vis.Network(container, data, options);
+    </script>
 
-</body>
-</html>
-"""
-    f.write(s)
-    f.close()
+    </body>
+    </html>
+    """
+    with open(file_name, 'w') as f:
+        f.write(s)
+        f.close()
