0dc00aa7509e11af42f717c79ca53d71e6c16408
didro <gmdidro@gmail.com>
2016-02-17 21:23:28 +0300

    Генератор последовательностей картинок

---
+++ apps/letter_test.py
@@ -0,0 +1,20 @@
+# -*- coding: utf8 -*-
+__author__ = 'AVPetrov'
+
+from gens.make_bubble import MakeBubble
+from temporalPooler.htm__region import Region as TemporalPoolerRegion
+from apps.settings import *
+
+temporal_settings = TemporalSettings(region_size=4, column_size=4, initial_permanence=0.5,
+                                     dendrite_activate_threshold=1, dendrite_permanence_inc_delta=0.02,
+                                     dendrite_permanence_dec_delta=-0.1, passive_time_to_active_threshold=1000,
+                                     synapse_threshold=0.45)
+input_settings = InputSettings(SCALE=1, STEPS_NUMBER=700, MAPPER=SquareMapperAutoRadius)
+generator = MakeBubble(TestSimpleSteps, temporal_settings.region_size, input_settings.SCALE)
+temporal_pooler_region = TemporalPoolerRegion(temporal_settings=temporal_settings)
+
+for i in range(input_settings.STEPS_NUMBER):
+    generator.out()
+    temporal_pooler_region.step_forward(generator.get_data())
+    temporal_pooler_region.out_prediction()
+    generator.move()
---
+++ gens/list_images_gen.py
@@ -0,0 +1,79 @@
+__author__ = 'bolotin'
+
+from PIL import Image
+
+class ListImages:
+    def __init__(self, square_size):
+        self.square_size = square_size
+        self.list_images = []
+        self.current_image = 0
+        self.count_images = 0
+        # border for binarization, will be load forward, default value
+        self.border = 100
+        self._load_config_()
+        self._load_images_()
+        if self.count_images == 0:
+            # make zero matr
+            print("WARNING in module 'list_image_generator.py' zero images was loaded")
+            self.list_images.append([ [0 for j in range(self.square_size)] for i in range(self.square_size)])
+
+    def _load_config_(self):
+        try:
+            f = open("htm-core\\gens\\Images\\config.txt","r")
+            self.border = int(f.read())
+            f.close()
+            if (self.border>255 or self.border<0):
+               raise Exception()
+        except Exception:
+             self.border = 100
+             print("EXCEPTION in module 'list_image_generator.py' while load config")
+        # finally:
+        #     f.close()
+    def _load_images_(self):
+        try:
+            f = open("htm-core\\gens\\Images\\order.txt","r")
+            for line in f:
+                #delete /n from end of line
+                if (line[len(line)-1] == '\n'):
+                    n_line = line[0:len(line)-1]
+                else :
+                    n_line = line
+                if len(n_line)>0:#non empty
+                    self._load_image_(n_line)
+            f.close()
+        except Exception:
+             print("EXCEPTION in module 'list_image_generator.py' no order file")
+        # finally:
+        #     f.close()
+    def _load_image_(self, file_name):
+        try:
+            self.list_images.append([])
+            number = self.count_images
+            Im = Image.open("htm-core\\gens\\Images\\" + file_name).resize((self.square_size,self.square_size))
+            size_x, size_y = Im.size
+            # result matr of bytes
+            for i in range(size_y):
+                 self.list_images[number].append([])
+                 for j in range(size_x):
+                    color = Im.getpixel((j,i))
+                    r,g,b = 0,1,2
+                    if (color[r]>self.border and color[g]>self.border and color[b]>self.border):
+                        self.list_images[number][i].append(1)
+                    else:
+                        self.list_images[number][i].append(0)
+            self.count_images += 1
+        except Exception:
+            # delete current matr from list
+            self.list_images.pop()
+            print("EXCEPTION in module 'list_image_generator.py' while load image '{}'".format(file_name))
+    def move(self):
+        self.current_image += 1
+        if (self.current_image >= self.count_images):
+            self.current_image = 0
+    def out(self):
+        print(self.current_image)
+        for i in self.list_images[self.current_image]:
+            print(i)
+        print()
+    def get_data(self):
+        return self.list_images[self.current_image]
